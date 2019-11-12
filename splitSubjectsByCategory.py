1cbcaf7860a80d7e94387faa4ee1710b5678e86b
Michelle Janowiecki <michelle@MSEL-CATMAC01.local>
2019-11-12 09:33:39 -0500

    Update splitSubjectsByCategory.py

--- splitSubjectsByCategory.py
+++ splitSubjectsByCategory.py
@@ -1,5 +1,6 @@
 import csv
 import argparse
+import re
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-f', '--file', help='enter filename with csv. optional - if not provided, the script will ask for input')
@@ -19,9 +20,13 @@ else:
 f = csv.writer(open('subjectsToCheckAgainstFASTAndMESH_Batch'+batch+'.csv', 'w'))
 f.writerow(['uri']+['dc.subject']+['cleanedSubject'])
 
+f2 = csv.writer(open('listSubjectsToCheckAgainstFASTAndMESH_Batch'+batch+'.csv', 'w'))
+f.writerow(['uri']+['dc.subject']+['cleanedSubject'])
+
 
 row_count = 0
 newrow_count = 0
+expectedvalue_count = 0
 
 with open(filename) as itemMetadataFile:
     itemMetadata = csv.DictReader(itemMetadataFile)
@@ -29,22 +34,29 @@ with open(filename) as itemMetadataFile:
         row_count = row_count + 1
         uri = row['uri']
         dc_subject = row['dc.subject']
-        newValue = row['newValue']
+        newValue = row['newValue'].strip()
         category = row['category'].strip()
         if category == 'list':
-            newValueList = newValue.split(',')
-            newValueCount = len(newValueList)
-            row_count = row_count + newValueCount
+            newValue = re.sub(',$', '', newValue) # removes comma from end of string
+            if newValue.count(',') > 0:
+                newValueList = newValue.split(',')
+            elif newValue.count(r'(\n|\r\n)') > 0:
+                newValueList = newValue.split(r'(\n|\r\n)')
+            else:
+                print("can't split!!")
+                print(newValue)
+            value_count = len(newValueList)
+            expectedvalue_count = expectedvalue_count + value_count
             for value in newValueList:
-                f.writerow([uri]+[dc_subject]+[value])
+                value = value.strip()
+                f2.writerow([uri]+[dc_subject]+[value])
                 newrow_count = newrow_count + 1
         elif category == 'remove':
             newValue = newValue.replace('.', '')
             f.writerow([uri]+[dc_subject]+[newValue])
-            newrow_count = newrow_count + 1
         else:
             f.writerow([uri]+[dc_subject]+[newValue])
-            newrow_count = newrow_count + 1
 
-print('total_row: '+str(row_count))
+print('total_original_rows: '+str(row_count))
+print('expected_count:'+str(expectedvalue_count))
 print('newrow_count: '+str(newrow_count))
